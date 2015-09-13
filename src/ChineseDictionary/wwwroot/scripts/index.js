6e31617111de4669fb73de01451271ecb270fbbe
Justin Ley <inkysigma@outlook.com>
2015-09-12 22:53:47 -0700

    Created the index to retrieve random characters and display.

--- src/ChineseDictionary/wwwroot/scripts/index.js
+++ src/ChineseDictionary/wwwroot/scripts/index.js
@@ -3,10 +3,28 @@
 
     app.controller("ListController", [
         '$scope', '$http', function($scope, $http) {
-            $scope.phrases = [];
-            $http({
-                url: window.location + '/api/Dictionary/GetRandom'
-            });
+            $scope.descriptions = [];
+            $http.post(window.domain + '/api/Dictionary/GetRandomRange')
+                .then(function(response) {
+                    for (var i = 0; i < response.data.length; i++) {
+                        var definition = "";
+                        for (var x = 0; x < response.data[i].Definition.length; i++) {
+                            definition.append(response.data[i].Definition[x] + ', ');
+                        }
+                        definition = definition.substring(0, definition.length - 2);
+                        $scope.descriptions.append({
+                            Word: response.data[i].Word,
+                            PartOfSpeech: response.data[i].PartOfSpeech,
+                            Definition: definition
+                    });
+                    }
+                }, function(response) {
+                    $scope.descriptions.append({
+                        Word: "An error occurred",
+                        PartOfSpeech: response.status,
+                        Definition: response.statusText
+                    });
+                });
         }
     ]);
 })
