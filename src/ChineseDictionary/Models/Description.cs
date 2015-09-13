59471b6516ab6a50734ec670f3cc76b6a9d9b73e
Justin Ley <inkysigma@outlook.com>
2015-09-13 11:59:09 -0700

    Added usages to description model

--- src/ChineseDictionary/Models/Description.cs
+++ src/ChineseDictionary/Models/Description.cs
@@ -8,27 +8,29 @@ namespace ChineseDictionary.Models
         public string Word { get; set; }
         public string PartOfSpeech { get; set; }
         public IEnumerable<string> Definitions { get; set; }
+        public IEnumerable<string> Usages { get; set; } 
 
-        public Description(string word, string partOfSpeech, IEnumerable<string> definitions)
+        public Description(string word, string partOfSpeech, IEnumerable<string> definitions, IEnumerable<string> usages)
         {
             Word = word;
             PartOfSpeech = partOfSpeech;
             Definitions = definitions;
+            Usages = usages;
         }
 
         public static Description Create(Character character)
         {
-            return new Description(character.Logograph, character.PartOfSpeech, character.Definition);
+            return new Description(character.Logograph, character.PartOfSpeech, character.Definition, character.Usages);
         }
 
         public static Description Create(Phrase phrase)
         {
-            return new Description(phrase.Word, phrase.PartOfSpeech, phrase.Definition);
+            return new Description(phrase.Word, phrase.PartOfSpeech, phrase.Definition, phrase.Usages);
         }
 
         public static Description Create(Idiom idiom)
         {
-            return new Description(idiom.Word, idiom.PartOfSpeech, idiom.Definition);
+            return new Description(idiom.Word, idiom.PartOfSpeech, idiom.Definition, idiom.Usages);
         }
     }
 }
