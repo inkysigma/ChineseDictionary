84e504de94698734624ad766ee0855f070a6c828
Justin Ley <inkysigma@outlook.com>
2015-09-13 12:15:01 -0700

    Added ability to add character.

--- src/ChineseDictionary/Controllers/CharacterController.cs
+++ src/ChineseDictionary/Controllers/CharacterController.cs
@@ -1,15 +1,31 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChineseDictionary.Resources.Managers;
+using ChineseDictionary.Resources.Models;
 using Microsoft.AspNet.Mvc;
+using Newtonsoft.Json;
 
 namespace ChineseDictionary.Controllers
 {
     public class CharacterController : Controller
     {
-        // GET: api/values
-        [HttpPost]
-        public IEnumerable<string> GetRandom()
+        private readonly ICharacterManager _characterManager;
+        public CharacterController(ICharacterManager characterManager)
         {
-            return new string[] { "value1", "value2" };
+            _characterManager = characterManager;
+        }
+        
+        [HttpPost]
+        public async Task<bool> AddCharacter(string data)
+        {
+            var obj = JsonConvert.DeserializeObject<Character>(data);
+            if (!obj.Validate())
+                return false;
+            if (!obj.Usages.Any())
+                return false;
+            await _characterManager.AddCharacterAsync(obj);
+            return true;
         }
     }
 }
