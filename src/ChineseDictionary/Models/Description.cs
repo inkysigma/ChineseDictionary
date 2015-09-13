5976b9a2f4c3d1dbdd1a3777565b61675ba2aec6
Justin Ley <inkysigma@outlook.com>
2015-09-13 12:06:53 -0700

    Added type metadata to Description

--- src/ChineseDictionary/Models/Description.cs
+++ src/ChineseDictionary/Models/Description.cs
@@ -5,6 +5,7 @@ namespace ChineseDictionary.Models
 {
     public class Description
     {
+        public string Type { get; set; }
         public string Word { get; set; }
         public string PartOfSpeech { get; set; }
         public IEnumerable<string> Definitions { get; set; }
@@ -20,17 +21,28 @@ namespace ChineseDictionary.Models
 
         public static Description Create(Character character)
         {
-            return new Description(character.Logograph, character.PartOfSpeech, character.Definition, character.Usages);
+            var description = new Description(character.Logograph, character.PartOfSpeech, character.Definition,
+                character.Usages)
+            {
+                Type = "Character"
+            };
+            return description;
         }
 
         public static Description Create(Phrase phrase)
         {
-            return new Description(phrase.Word, phrase.PartOfSpeech, phrase.Definition, phrase.Usages);
+            return new Description(phrase.Word, phrase.PartOfSpeech, phrase.Definition, phrase.Usages)
+            {
+                Type = "Phrase"
+            };
         }
 
         public static Description Create(Idiom idiom)
         {
-            return new Description(idiom.Word, idiom.PartOfSpeech, idiom.Definition, idiom.Usages);
+            return new Description(idiom.Word, idiom.PartOfSpeech, idiom.Definition, idiom.Usages)
+            {
+                Type = "Idiom"
+            };
         }
     }
 }
