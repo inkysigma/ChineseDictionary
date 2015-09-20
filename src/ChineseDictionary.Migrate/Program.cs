f5bc0b2edfed352f75f2f42365818742940b7ec1
inkybeta <inkybeta@outlook.com>
2015-09-20 00:57:48 -0700

    Fixed migrate tool

--- src/ChineseDictionary.Migrate/Program.cs
+++ src/ChineseDictionary.Migrate/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+#if DNX451
+using System.Diagnostics;
+#endif
 using System.IO;
 using System.Threading.Tasks;
 
@@ -17,16 +19,52 @@ namespace ChineseDictionary.Migrate
             {
                 text.Add(await stream.ReadLineAsync());
             }
-            text.Insert(text.Count - 3, "\t\tprotected override void OnConfiguring(DbContextOptionsBuilder builder)");
-            text.Insert(text.Count - 3, "\t\t{");
-            text.Insert(text.Count - 3, "\t\t\tbuilder.UseNpgsql(\"Server = localhost; Database = chinese; User Id = default; Password =public\");");
-            text.Insert(text.Count - 3, "\t\t}");
+            stream.Dispose();
+            var namespaceIndexLine = text.IndexOf("namespace ChineseDictionary.Resources");
+            text[namespaceIndexLine] = "namespace ChineseDictionary.temp";
+            text.Insert(text.Count - 3, "\tprotected override void OnConfiguring(DbContextOptionsBuilder builder)");
+            text.Insert(text.Count - 3, "\t{");
+            text.Insert(text.Count - 3, "\t\tbuilder.UseNpgsql(\"Server = localhost; Database = chinese; User Id = default; Password =public\");");
+            text.Insert(text.Count - 3, "\t}");
             var writer = new StreamWriter("temp/DictionaryContext.cs");
             foreach (var t in text)
             {
-                writer.Write(t);
+                writer.WriteLine(t);
             }
             await writer.FlushAsync();
+            writer.Dispose();
+
+            Console.WriteLine("Enter the name of the migration:");
+            var migrationName = Console.ReadLine();
+            var info = new ProcessStartInfo("dnx", "ef migrations add " + migrationName);
+            info.CreateNoWindow = true;
+            info.RedirectStandardOutput = true;
+            info.RedirectStandardError = true;
+            info.UseShellExecute = false;
+            var add = Process.Start(info);
+            while (!add.StandardError.EndOfStream)
+                Console.WriteLine(await add.StandardError.ReadLineAsync());
+            add.WaitForExit();
+            info = new ProcessStartInfo("dnx", "ef database update");
+            info.CreateNoWindow = true;
+            info.RedirectStandardError = true;
+            info.RedirectStandardOutput = true;
+            var update = Process.Start(info);
+            while (!update.StandardOutput.EndOfStream || !update.StandardError.EndOfStream)
+            {
+                if (!update.StandardOutput.EndOfStream)
+                {
+                    Console.WriteLine(await update.StandardOutput.ReadLineAsync());
+                }
+
+                if (!update.StandardError.EndOfStream)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(await update.StandardError.ReadLineAsync());
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+            update.WaitForExit();
         }
     }
 }
